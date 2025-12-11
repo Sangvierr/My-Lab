2983a6005065ee8b7722c4e4d5b7b1b3896ce110
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2025-12-12 00:07:19 +0900

    Refactor: Fix test code structure

---
+++ mylab-data-pipeline/src/test/java/com/mylab/MylabDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // 이제 MylabDataPipelineApplication을 찾을 수 있습니다!
+class MylabDataPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
